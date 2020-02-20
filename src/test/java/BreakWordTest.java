ee5f4b579b68bc5e8af3c068dd1daabec4511050
SoulGuard <653330836@qq.com>
2020-02-20 11:38:20 +0800

    [update]添加注释

--- src/test/java/BreakWordTest.java
+++ src/test/java/BreakWordTest.java
@@ -11,11 +11,15 @@ public class BreakWordTest {
     @Test
     public void breakWordTest() {
         BreakWord bw = new BreakWord();
-        String str = "ilikesamsungmobile";
+        String str = "ilikesamsungmobile1";
         Set dict = new HashSet();
         List<String> lit = bw.wordBreak(str,dict,null);
         System.out.println("********普通字典输出***********");
+        if(lit.size()==0){
+            System.out.println("输入的字符串存在部分单词不在字典里");
+        }
         lit.stream().forEach(outputStr -> System.out.println(outputStr));
+
     }
 
 
@@ -30,6 +34,9 @@ public class BreakWordTest {
         dict.add("tv");
         List<String> lit = bw.wordBreak(str,dict,"single");
         System.out.println("********使用自定义字典输出***********");
+        if(lit.size()==0){
+            System.out.println("输入的字符串存在部分单词不在字典里");
+        }
         lit.stream().forEach(outputStr -> System.out.println(outputStr));
     }
 
@@ -44,6 +51,9 @@ public class BreakWordTest {
         dict.add("books");
         List<String> lit = bw.wordBreak(str,dict,"all");
         System.out.println("********默认字典与自定义字典合并字典输出***********");
+        if(lit.size()==0){
+            System.out.println("输入的字符串存在部分单词不在字典里");
+        }
         lit.stream().forEach(outputStr -> System.out.println(outputStr));
     }
 
